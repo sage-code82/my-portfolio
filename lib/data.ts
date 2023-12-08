1a43d8e6c67b2004c5c20480a0ef758a216ba2cb
Stephen <sagelvl40@gmail.com>
2023-12-08 16:18:41 +0000

    add projects section

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-// import corpcommentImg from "@/public/corpcomment.png";
-// import rmtdevImg from "@/public/rmtdev.png";
-// import wordanalyticsImg from "@/public/wordanalytics.png";
+import pantrypetImg from "@/public/pantrypet.png";
+import blobsinspaceImg from "@/public/blobsinspace.png";
+import dndcompanionImg from "@/public/dndcompanion.png";
 
 export const links = [
   {
@@ -62,30 +62,31 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "PantryPet",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    // imageUrl: corpcommentImg,
+      "Team project during bootcamp. Keep track of your groceries and get meal time inspiration!",
+    tags: ["HTML", "CSS", "JavaScript"],
+    imageUrl: pantrypetImg,
   },
   {
-    title: "rmtDev",
+    title: "BlobsInSpace",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    // imageUrl: rmtdevImg,
+      "Starting as simple Asteroids clone, planning on expanding it to much more!",
+    tags: ["HTML", "CSS", "JavaScript"],
+    imageUrl: blobsinspaceImg,
   },
   {
-    title: "Word Analytics",
+    title: "DnD Companion",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    // imageUrl: wordanalyticsImg,
+      "My final bootcamp project. A players companion app for the game Dungeons and Dragons. Create your character and keep an ongoing journal about your game!",
+    tags: ["MongoDB", "Express", "React", "Node.js", "Acordion", "CSS"],
+    imageUrl: dndcompanionImg,
   },
 ] as const;
 
 export const skillsData = [
   "HTML",
+  "HTML Canvas",
   "CSS",
   "JavaScript",
   "TypeScript",
@@ -94,14 +95,9 @@ export const skillsData = [
   "Node.js",
   "Git",
   "Tailwind",
-  "Prisma",
   "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
   "Express",
-  "PostgreSQL",
-  "Python",
-  "Django",
   "Framer Motion",
+  "Acordion",
+  "Chart.js",
 ] as const;
