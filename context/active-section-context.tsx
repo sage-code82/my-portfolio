265d21e30b01387c72e1001f74e996fedc3810b8
Stephen <sagelvl40@gmail.com>
2023-12-11 11:03:18 +0000

    finish the header make hook for header scroll to make code dry

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,7 +1,7 @@
-import React, { useState, createContext } from "react";
-import { links } from "@/lib/data";
+"use client";
 
-type SectionName = (typeof links)[number]["name"];
+import React, { useState, createContext, useContext } from "react";
+import { SectionName } from "@/lib/types";
 
 type ActiveSectionContextProviderProps = {
   children: React.ReactNode;
@@ -10,24 +10,39 @@ type ActiveSectionContextProviderProps = {
 type ActiveSectionContextType = {
   activeSection: SectionName;
   setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+  timeOfLastClick: number;
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
 };
 
-const ActiveSectionContext = createContext<ActiveSectionContextType | null>(
-  null
-);
+export const ActiveSectionContext =
+  createContext<ActiveSectionContextType | null>(null);
 
 export default function ActiveSectionContextProvider({
   children,
 }: ActiveSectionContextProviderProps) {
   const [activeSection, setActiveSection] = useState<SectionName>("Home");
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0);
   return (
     <ActiveSectionContext.Provider
       value={{
         activeSection,
         setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick,
       }}
     >
       {children}
     </ActiveSectionContext.Provider>
   );
+}
+
+export function useActiveSectionContext() {
+  const context = useContext(ActiveSectionContext);
+
+  if (context === null) {
+    throw new Error(
+      "useActiveSectionContext must be used within an ActiveSectionProvider"
+    );
+  }
+  return context;
 }
