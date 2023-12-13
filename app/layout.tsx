a9cd198fe957efb9f4fc6fab1fcd2bf7591b8864
Stephen <sagelvl40@gmail.com>
2023-12-13 22:57:11 +0000

    update the json package

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         className={`${inter.className} bg-slate-50 text-slate-950 relative pt-28 sm:pt-28 dark:bg-slate-900 dark:text-slate-50 dark:text-opacity-90`}
       >
         <div className="bg-[#ede2fb] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
-        ;
+
         <div className="bg-[#d7eefb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,7 @@ import React from "react";
 import { AiTwotoneTrophy } from "react-icons/ai";
 import { LuGraduationCap } from "react-icons/lu";
 import { PiCatDuotone } from "react-icons/pi";
+import { TbBrandPython } from "react-icons/tb";
 import pantrypetImg from "@/public/pantrypet.png";
 import blobsinspaceImg from "@/public/blobsinspace.png";
 import dndcompanionImg from "@/public/dndcompanion.png";
@@ -58,22 +59,32 @@ export const upcomingData = [
     icon: React.createElement(PiCatDuotone),
     date: "January 2024",
   },
+  {
+    title: "Learning Python",
+    location: "Python",
+    description:
+      "Self teaching myself Python with the help of FreeCodeAcademy and YouTube tutorials",
+    icon: React.createElement(TbBrandPython),
+    date: "January 2024",
+  },
 ] as const;
 
 export const projectsData = [
-  {
-    title: "PantryPet",
-    description:
-      "Team project during bootcamp. Keep track of your groceries and get meal time inspiration!",
-    tags: ["HTML", "CSS", "JavaScript"],
-    imageUrl: pantrypetImg,
-  },
   {
     title: "BlobsInSpace",
     description:
       "Starting as simple Asteroids clone, planning on expanding it to much more!",
     tags: ["HTML", "CSS", "JavaScript"],
     imageUrl: blobsinspaceImg,
+    githubUrl: "https://github.com/sage-code82/blobsinspace",
+  },
+  {
+    title: "PantryPet",
+    description:
+      "Team project during bootcamp. Keep track of your groceries and get meal time inspiration!",
+    tags: ["HTML", "CSS", "JavaScript"],
+    imageUrl: pantrypetImg,
+    githubUrl: "https://github.com/nellie-allsop/pantry",
   },
   {
     title: "DnD Companion",
@@ -81,6 +92,7 @@ export const projectsData = [
       "My final bootcamp project. A players companion app for the game Dungeons and Dragons. Create your character and keep an ongoing journal about your game!",
     tags: ["MongoDB", "Express", "React", "Node.js", "CSS"],
     imageUrl: dndcompanionImg,
+    githubUrl: "https://github.com/MannyGGB/dnd-companion",
   },
 ] as const;
 
