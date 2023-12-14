ee93751eca4de008571ab1fcf754371fa5ea3f5e
Stephen <sagelvl40@gmail.com>
2023-12-14 14:24:09 +0000

    work on upcoming to fix deployment error

--- components/about.tsx
+++ components/about.tsx
@@ -20,24 +20,24 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-4">
         Enthusiastic bootcamp graduate with a focus on the{" "}
-        <span className="font-medium">MERN stack</span>. I&aposve decided to
-        turn my passion for programming into a professional journey, embarking
-        on a career in full-stack web development. The thrill of problem-solving
-        is my favorite aspect of programming—I love the satisfaction of finding
+        <span className="font-medium">MERN stack</span>. I have decided to turn
+        my passion for programming into a professional journey, embarking on a
+        career in full-stack web development. The thrill of problem-solving is
+        my favorite aspect of programming—I love the satisfaction of finding
         solutions. My core stack includes{" "}
         <span className="font-medium">
           MongoDB, Express.js, React, and Node.js
         </span>
-        . I&aposm always eager to learn and continually enhance my skills
-        through platforms like FreeCodeAcademy and YouTube. Currently seeking a{" "}
+        . Always eager to learn and continually enhance my skills through
+        platforms like FreeCodeAcademy and YouTube. Currently seeking a{" "}
         <span className="font-medium">junior software developer position</span>{" "}
         and open to freelance opportunities to expand my portfolio.
       </p>
 
       <p>
-        <span className="italic">When I&aposm not coding</span>, you will find
-        me immersed in tabletop gaming and various hobby-related activities. I
-        am passionate about continual learning, currently delving into{" "}
+        <span className="italic">When I am not coding</span>, you will find me
+        immersed in tabletop gaming and various hobby-related activities. I am
+        passionate about continual learning, currently delving into{" "}
         <span className="font-medium">AI and prompt engineering</span>. I&aposve
         also explored content creation through my blogs and a YouTube channel,
         experimenting with sharing my insights and experiences.
