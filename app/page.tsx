40247c1cff5affa22ba28c083e823bc9728c1def
Stephen <sagelvl40@gmail.com>
2023-12-14 23:59:30 +0000

    back to normal try again another time

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Intro from "@/components/intro";
 import SectionDivider from "@/components/section-divider";
 import About from "@/components/about";
@@ -10,20 +9,6 @@ import Contact from "@/components/contact";
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
-      <Head>
-        <title>Your Home Page Title</title>
-        <meta property="og:title" content="Your Home Page Title" />
-        <meta property="og:description" content="Your home page description." />
-        <meta property="og:image" content="/link_pic.png" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Your Home Page Title" />
-        <meta
-          name="twitter:description"
-          content="Your home page description."
-        />
-        <meta name="twitter:image" content="/link_pic.png" />
-      </Head>
       <Intro />
       <SectionDivider />
       <About />
