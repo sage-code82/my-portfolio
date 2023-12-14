19a68ba40398142084727983c31fea8f793b1ea0
Stephen <sagelvl40@gmail.com>
2023-12-14 22:33:12 +0000

    trail the meta image data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Intro from "@/components/intro";
 import SectionDivider from "@/components/section-divider";
 import About from "@/components/about";
@@ -9,6 +10,20 @@ import Contact from "@/components/contact";
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
+      <Head>
+        <title>Your Home Page Title</title>
+        <meta property="og:title" content="Your Home Page Title" />
+        <meta property="og:description" content="Your home page description." />
+        <meta property="og:image" content="/public/link_pic.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Your Home Page Title" />
+        <meta
+          name="twitter:description"
+          content="Your home page description."
+        />
+        <meta name="twitter:image" content="/public/link_pic.png" />
+      </Head>
       <Intro />
       <SectionDivider />
       <About />
